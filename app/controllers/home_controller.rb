39178aaf8cda6ad13dfbb42e73a7c46d10196c8b
Ross Foley <admin@makeamarkr10.com>
2010-03-23 19:28:05 -0400

    Update most files

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.all
-    @files = Nspirefile.find(:all, :limit => 15)
+    @posts = Post.find(:all, :conditions => ["post_type = :posttype",{:posttype => "home"}], :order => "created_at DESC")
+    @files = Nspirefile.find(:all, :limit => 15, :order => "id")
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-$(document).ready(function() {
-	$("li").hover(
-	function() {
-		$(this).addClass("navmenuitemhover");
-	},
-	function() {
-		$(this).removeClass("navmenuitemhover");
+
+$(function() {
+	var input_field = $("#image_edit_field");
+	input_field.focusout(function() {
+		$("#image_preview_field").attr("src", "/images/nspire/"+input_field.val());
 	});
 });
