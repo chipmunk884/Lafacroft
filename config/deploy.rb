27b56bbb9bc61c8c6fe716277ff90d0693606971
Ross Foley <ross3141@gmail.com>
2010-04-04 17:35:02 -0400

    Update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-
+#Site5 specific stuff
 set :use_sudo, false
 set :group_writable, false
  
@@ -6,61 +6,45 @@ set :group_writable, false
 set :keep_releases, 2
  
 # The mandatory stuff
-set :application, "YOUR_APP_NAME"
-set :user, "SSH_USERNAME"
- 
-set :repository,  "URL_FOR_YOUR_REPOSITORY"
- 
-# SCM information
-set :scm_username, "SCM_USERNAME"
-set :scm_password, Proc.new { CLI.password_prompt "SVN Password: "}
+set :application, "lafacroft"
+set :user, "lafacrof"
+
+#Git info
+default_run_options[:pty] = true
+set :repository,  "git://github.com/chipmunk884/Lafacroft.git"
+set :scm, "git"
+set :branch, "master"
+set :deploy_via, :remote_cache
  
 # This is related to site5 too.
-set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_to, "/home/#{user}/#{application}"
+role :app, "lafacroft.com"
+role :web, "lafacroft.com"
+role :db,  "lafacroft.com", :primary => true
  
-role :app, "SERVERNAME"
-role :web, "SERVERNAME"
-role :db,  "SERVERNAME", :primary => true
- 
- 
-# In the deploy namespace we override some default tasks and we define
-# the site5 namespace.
+# Create Site5 specific tasks
 namespace :deploy do
-  desc <<-DESC
-    Deploys and starts a `cold' application. This is useful if you have not \
-    deployed your application before, or if your application is (for some \
-    other reason) not currently running. It will deploy the code, run any \
-    pending migrations, and then instead of invoking `deploy:restart', it will \
-    invoke `deploy:start' to fire up the application servers.
-  DESC
-  # NOTE: we kill public_html so be sure to have a backup or be ok with this application
-  # being the default app for the domain.
+  desc "Deploy app, run migrations, and start Phusion Passenger"
   task :cold do
     update
     site5::link_public_html
-    site5::kill_dispatch_fcgi
+    restart
   end
  
-  desc <<-DESC
-    Site5 version of restart task.
-  DESC
+  desc "Restart Phusion Passenger"
   task :restart do
-    site5::kill_dispatch_fcgi
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  
+  desc "Start application -- not needed for Passenger"
+  task :start, :roles => :app do
+    #nothing, needs to be blank to make Passenger work properly
   end
  
   namespace :site5 do
-    desc <<-DESC
-      Links public_html to current_release/public
-    DESC
+    desc "Link the public folder of the application to public_html"
     task :link_public_html do
       run "cd /home/#{user}; rm -rf public_html; ln -s #{current_path}/public ./public_html"
-    end
- 
-    desc <<-DESC
-      Kills Ruby instances on Site5
-    DESC
-    task :kill_dispatch_fcgi do
-      run "skill -u #{user} -c ruby"
     end
   end
 end
