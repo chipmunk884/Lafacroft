4ae062ac43022dd103eff60b9e2f7397d7be8f85
Ross Foley <ross3141@gmail.com>
2010-04-12 20:11:40 -0400

    Fixed "Latest Files" section on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @posts = Post.all(:conditions => ["post_type = ?","home"], :order => "created_at DESC")
-    @files = Nspirefile.find(:all, :limit => 15, :order => "id")
+    @files = Nspirefile.find(:all, :limit => 15, :order => "created_at DESC")
   end
 end
